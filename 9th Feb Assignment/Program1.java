c1c8547a27a103a8349bd1b7211e9b502483df65
Geekixamit <amitsikarwar1999@gmail.com>
2022-02-13 15:38:57 +0530

    First Program

---
+++ 9th Feb Assignment/Program1.java
@@ -0,0 +1,15 @@
+public class Program1 
+{ 
+    public static void main(String[] args) 
+    {
+        int num1 = 10, num2 = 20;
+
+        if(num1 > num2){
+            System.out.println("Maximum number is " + num1);
+        } else if(num1 < num2) {
+            System.out.println("Maximum number is " + num2);
+        } else {
+            System.out.println("Both numbers are same!");
+        }
+    }
+}
